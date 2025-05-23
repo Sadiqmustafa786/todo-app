7ed66bc51120f912790192a1e84bea393fefeb1e
Sadiq Mustafa <sadiqmustafa906@gmail.com>
2025-05-23 23:03:30 +0500

    Updated LandingPage UI, Tailwind config, and global styles

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -14,27 +14,27 @@ const LandingPage = () => {
   }, [user, navigate]);
 
   return (
-    <div className="min-h-screen flex flex-col-reverse md:flex-row items-center overflow- justify-between bg-gradient-to-br from-orange-100 to-yellow-200 px-8 pt-24 md:pt-0 animate-fade-in">
+    <div className="min-h-screen flex flex-col-reverse md:flex-row items-center overflow- justify-between bg-white px-8 pt-24 md:pt-0 animate-fade-in">
       {/* Left Section */}
       <div className="md:w-1/2 text-center md:text-left md:mt-0 mt-6 md:mb-20 space-y-6">
-        <h1 className="md:text-4xl text-3xl font-extrabold text-gray-800">
+        <h1 className="font-prata md:text-4xl text-3xl font-extrabold text-gray-800">
           Organize Your Day with{" "}
-          <span className="text-orange-500">Todo App</span>
+          <span className="text-orange-500 font-prata">Todo App</span>
         </h1>
-        <p className="text-gray-600 text-lg">
+        <p className="font-prata text-gray-600 text-lg">
           Keep track of your tasks, stay productive, and simplify your life.
         </p>
         <div className="flex justify-center md:justify-start gap-4">
           <Link
             to="/register"
-            className="bg-orange-500 text-white px-6 py-2 rounded-lg hover:bg-orange-600 transition-all duration-300 ease-in-out transform hover:scale-105"
+            className="font-prata bg-orange-500 text-white px-6 py-2 rounded-lg hover:bg-white  hover:text-orange-500  border-2 hover:border-orange-500 transition-all duration-300 ease-in-out transform hover:scale-105"
             aria-label="Get Started"
           >
             Get Started
           </Link>
           <Link
             to="/login"
-            className="bg-white text-orange-500 border-2 border-orange-500 px-6 py-2 rounded-lg hover:bg-orange-100 transition-all duration-300 ease-in-out transform hover:scale-105"
+            className="bg-white text-orange-500 border-2 border-orange-500 px-6 py-2 rounded-lg hover:bg-orange-500 hover:text-white transition-all duration-300 ease-in-out transform hover:scale-105"
             aria-label="Login"
           >
             Login
